498120094bb339cebec2435ae03db487cd0fef80
Aartijamadade <126068558+Aartijamadade@users.noreply.github.com>
2023-03-31 18:57:16 +0530

    Update Aarti.py

--- output/Aarti.py
+++ output/Aarti.py
@@ -9,7 +9,7 @@ zip_file_name = "output_1_0.zip"
 z=ZipFile("Output_1_0.zip",'w')
 
 z.write("zip.py")
-z.write("test.py")
+z.write("test.py ")
 z.write("text.txt")
 z.write("test1.py")
 z.write("text2.txt")
