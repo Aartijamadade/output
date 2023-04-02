99c276a8ad9667c898dec08a2f9c9fdd64639dd3
Aartijamadade <126068558+Aartijamadade@users.noreply.github.com>
2023-04-02 10:12:28 +0530

    Delete output directory

--- output/output/Aarti.py
+++
@@ -1,8 +0,0 @@
-from zipfile import ZipFile
-
-z=ZipFile("Output_1_0.zip",'w')
-
-z.write("zip.py")
-z.write("test.py")
-z.write("text.txt")
-z.close()
--- output/output/test.py
+++
@@ -1,4 +0,0 @@
-a=input(" enter the first no")
-b=input(" enter the second no")
-c=int(a)+int(b)
-print("addition",c)
